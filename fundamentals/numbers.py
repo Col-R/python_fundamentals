a91e8417592f2f072c153bde01bfe1c7528276e0
Col-R <2colewestin@gmail.com>
2021-09-01 17:38:41 -0500

    number methods

---
+++ fundamentals/numbers.py
@@ -0,0 +1,16 @@
+# There are 3 basics types of numbers in Python.
+
+# int - whole numbers, positive or negative.  ex. 35
+# float - decimal numbers, positive or negative.  ex. 4.2
+# complex - are a part of the real number system and are often referenced with the letter j.  ex. 1 + 3j.  **Note** If you're not sure if you need to use them, it's safe to say you can ignore this data type.
+
+#int
+num = 10
+#float
+dec = 6.1
+#log
+print(num)
+#type check
+print (type(dec))
+#conversion
+num_to_dec = float(num)
