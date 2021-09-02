56a270e135d63d2ccf9406b295460a075d17f76b
Col-R <2colewestin@gmail.com>
2021-09-02 06:33:18 -0500

    created tuples notes

---
+++ fundamentals/tuples.py
@@ -0,0 +1,9 @@
+# A tuple is kind of like an object. Has multiple values tied to one variable
+# Cannot be modified or changed.
+
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+print(dog[2])
