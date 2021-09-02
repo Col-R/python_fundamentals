77648229ec62d3a4553b7b8347b5c1176170af0e
Col-R <2colewestin@gmail.com>
2021-09-01 19:13:21 -0500

    creates strings file

---
+++ fundamentals/strings.py
@@ -0,0 +1,13 @@
+#string literals
+print('this is a sample string')
+
+#concatenation - The print() function inserts a space between elements separated by a comma.
+name = "Zen"
+print("My name is", name)
+#The second is by concatenating the contents into a new string, with the help of +.
+name = "Zen"
+print("My name is " + name)
+
+number = 5
+print('My number is', number)
+# print('My number is'+ number) throws an error
