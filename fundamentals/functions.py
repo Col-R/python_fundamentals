c46151dd2ba9fe44fa9c3090bba90717eb460466
Col-R <2colewestin@gmail.com>
2021-09-03 06:39:37 -0500

    functions notes

---
+++ fundamentals/functions.py
@@ -0,0 +1,28 @@
+# return vs print
+
+# define a function
+# def myFunction():
+#     print('hello')
+
+# call the function
+# print(myFunction())
+
+# Every function returns something
+
+# def a():
+#     return 5
+# x = a()
+# print(x + 10)
+
+# Parameter vs argument
+def introduction(name): # (name) parameter
+    print(f'Hello, my name is {name}')
+introduction('Col') #Col is argument
+
+def introduction(name, food='ramen'):
+    print(f'Hello, my name is {name} and I like {food}')
+introduction('Col')
+
+def introduction(food, name = "Col"):
+    print(f'Hello, my name is {name} and I like {food}')
+introduction('ramen')
