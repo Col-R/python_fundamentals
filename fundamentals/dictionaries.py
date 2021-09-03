f2d5562445bb776e20027366b2c4bbfb4a9b3b2f
Col-R <2colewestin@gmail.com>
2021-09-02 19:40:33 -0500

    updates dictionaries

--- fundamentals/dictionaries.py
+++ fundamentals/dictionaries.py
@@ -13,14 +13,21 @@ print(capitals["svk"])
 value_removed = capitals.pop('svk') # will remove the key 'svk' and return it's value
 del capitals['dnk'] # will delete the key, and not return anything
 
+#changing values
+weekend['Sunday'] = 'S'
+print(weekend)
+
 # nested dictionaries
 context = {
-    'questions': [
-        { 'id': 1, 'content': 'Why is there a light in the fridge and not in the freezer?'},
-        { 'id': 2, 'content': 'Why don\'t sheep shrink when it rains?'},
-        { 'id': 3, 'content': 'Why are they called apartments when they are all stuck together?'},
-        { 'id': 4, 'content': 'Why do cars drive on the parkway and park on the driveway?'}
-    ]
+    'questions': [
+        {'id': 1, 'content': 'Why is there a light in the fridge and not in the freezer?'},
+        {'id': 2, 'content': 'Why don\'t sheep shrink when it rains?'},
+        {'id': 3, 'content': 'Why are they called apartments when they are all stuck together?'},
+        {'id': 4, 'content': 'Why do cars drive on the parkway and park on the driveway?'}
+    ]
 }
 
 # yourDictionary.method() for methods
+# .keys()
+# .values()
+# .items()
