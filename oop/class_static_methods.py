031f686ea29f450f414783b328ebaab07cd49765
Col-R <2colewestin@gmail.com>
2021-09-06 15:41:42 -0500

    added static methods

--- oop/class_static_methods.py
+++ oop/class_static_methods.py
@@ -1,4 +1,6 @@
 # Class methods are defined with a decorator @classmethod. They belong to the class itself instead of the instance. Instead of implicitly passing self into the method, we pass cls. This is reference to the class.
+# Static methods are functions defined within the class with a decorator @staticmethod.  They have no access on instance or class attributes so we do need to pass any arguments into them.
+
 class BankAccount:
     # class attribute
     bank_name = "First National Dojo"
@@ -7,7 +9,24 @@ class BankAccount:
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.all_accounts.append(self)
-    
+
+    def with_draw(self,amount):
+        # we can use the static method here to evaluate
+        # if we can with draw the funds without going negative
+        if BankAccount.can_withdraw(self.balance,amount):
+            self.balance -= amount
+        else:
+            print("Insufficient Funds")
+        return self    
+    # static methods have no access to any attribute
+    # only to what is passed into it
+    @staticmethod
+    def can_withdraw(balance,amount):
+    	if (balance - amount) < 0:
+	    return False
+        else:
+	    return True
+
     # class method to change the name of the bank
     @classmethod
     def change_bank_name(cls,name):
