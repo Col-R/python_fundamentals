a42f1cdfdc1273c4c5e8252014758aa071fef004
Col-R <2colewestin@gmail.com>
2021-09-06 16:22:23 -0500

    added classmethod to view all account balances

--- assignments/BankAccount/bank_account.py
+++ assignments/BankAccount/bank_account.py
@@ -1,8 +1,10 @@
 class BankAccount:
+    accounts = []
     def __init__(self, int_rate, balance):
         # Make user class later
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
@@ -23,8 +25,15 @@ class BankAccount:
             self.balance = self.balance + (self.int_rate * self.balance)
         return self
 
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
 acc1 = BankAccount(.02, 1000)
-acc1.deposit(300).deposit(250).deposit(500).withdraw(80).yield_interest().display_account_info()
+# acc1.deposit(300).deposit(250).deposit(500).withdraw(80).yield_interest().display_account_info()
 
 acc2 = BankAccount(.02, 2000)
-acc2.deposit(100).deposit(650).withdraw(150).withdraw(800).withdraw(425).withdraw(390).yield_interest().display_account_info()
+# acc2.deposit(100).deposit(650).withdraw(150).withdraw(800).withdraw(425).withdraw(390).yield_interest().display_account_info()
+
+BankAccount.print_all_accounts()
