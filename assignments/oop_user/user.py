df93b599f9e32989dec6507b21030bd346e29203
Col-R <2colewestin@gmail.com>
2021-09-06 15:14:28 -0500

    updated to include method chaining

--- assignments/oop_user/user.py
+++ assignments/oop_user/user.py
@@ -5,37 +5,29 @@ class User:
     def make_withdrawal(self, amount):
         self.balance -= amount
         print(f"{self.name}, you have withdrawn ${amount}")
+        return self
     def make_deposit(self, amount):
         self.balance += amount
         print(f"{self.name}, you have deposited ${amount}")
+        return self
     def display_user_balance(self):
         print(f"{self.name}, your balance is ${self.balance}")
+        return self
     def transfer_money(self, target, amount):
         self.balance -= amount
         target.balance += amount
         print(f"{self.name}, your balance is ${self.balance}")
         print(f"{target.name}, your balance is ${target.balance}")
+        return self
 
 hamilton = User('hamilton', 1000)
 peggy = User('peggy', 810)
 hercules = User('hercules', 450)
 
-hamilton.make_deposit(55)
-hamilton.make_deposit(55)
-hamilton.make_deposit(55)
-hamilton.make_withdrawal(250)
-hamilton.display_user_balance()
+hamilton.make_deposit(55).make_deposit(55).make_deposit(55).make_withdrawal(250).display_user_balance()
 
-peggy.make_deposit(25)
-peggy.make_deposit(38)
-peggy.make_withdrawal(12)
-peggy.make_withdrawal(80)
-peggy.display_user_balance()
+peggy.make_deposit(25).make_deposit(38).make_withdrawal(12).make_withdrawal(80).display_user_balance()
 
-hercules.make_deposit(310)
-hercules.make_withdrawal(35)
-hercules.make_withdrawal(190)
-hercules.make_withdrawal(80)
-hercules.display_user_balance()
+hercules.make_deposit(310).make_withdrawal(35).make_withdrawal(190).make_withdrawal(80).display_user_balance()
 
 hamilton.transfer_money(hercules, 250)
