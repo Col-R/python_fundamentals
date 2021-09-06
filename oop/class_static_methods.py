4e6e71b8da0ea14ed01122e8e42a47f00cdd50cd
Col-R <2colewestin@gmail.com>
2021-09-06 16:17:19 -0500

    updated syntax

--- oop/class_static_methods.py
+++ oop/class_static_methods.py
@@ -2,40 +2,41 @@
 # Static methods are functions defined within the class with a decorator @staticmethod.  They have no access on instance or class attributes so we do need to pass any arguments into them.
 
 class BankAccount:
-    # class attribute
-    bank_name = "First National Dojo"
-    all_accounts = []
-    def __init__(self, int_rate,balance):
-        self.int_rate = int_rate
-        self.balance = balance
-        BankAccount.all_accounts.append(self)
+    # class attribute
+    bank_name = "First National Dojo"
+    all_accounts = []
+    def __init__(self, int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.all_accounts.append(self)
 
-    def with_draw(self,amount):
-        # we can use the static method here to evaluate
-        # if we can with draw the funds without going negative
-        if BankAccount.can_withdraw(self.balance,amount):
-            self.balance -= amount
-        else:
-            print("Insufficient Funds")
-        return self    
-    # static methods have no access to any attribute
-    # only to what is passed into it
+    def with_draw(self,amount):
+        # we can use the static method here to evaluate
+        # if we can with draw the funds without going negative
+        if BankAccount.can_withdraw(self.balance,amount):
+            self.balance -= amount
+        else:
+            print("Insufficient Funds")
+            return self
+    # static methods have no access to any attribute
+    # only to what is passed into it
     @staticmethod
     def can_withdraw(balance,amount):
-    	if (balance - amount) < 0:
-	    return False
+        if (balance - amount) < 0:
+            return False
         else:
-	    return True
+            return True
 
-    # class method to change the name of the bank
-    @classmethod
-    def change_bank_name(cls,name):
-        cls.bank_name = name
-    # class method to get balance of all accounts
-    @classmethod
-    def all_balances(cls):
-        sum = 0
-        # we use cls to refer to the class
-        for account in cls.all_accounts:
-            sum += account.balance
-        return sum
+    # class method to change the name of the bank
+    @classmethod
+    def change_bank_name(cls,name):
+        cls.bank_name = name
+    # class method to get balance of all accounts
+    @classmethod
+    def all_balances(cls):
+        sum = 0
+        # we use cls to refer to the class
+        for account in cls.all_accounts:
+            sum += account.balance
+        return sum
+print(BankAccount.all_balances())
