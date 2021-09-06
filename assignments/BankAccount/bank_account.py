1242d4768abe6597c4b0f1411de42e10dbc213e3
Col-R <2colewestin@gmail.com>
2021-09-06 15:48:15 -0500

    created class framework

---
+++ assignments/BankAccount/bank_account.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        # Make user class later
+        self.int_rate = int_rate
+        self.balnce = balance
+    def deposit(self, amount):
+        pass
+    def withdraw(self,amount):
+        pass
+    def display_account_info(self):
+        pass
+    def yield_interest(self):
+        pass
