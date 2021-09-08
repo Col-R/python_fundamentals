0dff49cc788ef454a7b80eb15c185d16c5bcdf93
Col-R <2colewestin@gmail.com>
2021-09-08 06:54:46 -0500

    completed modularizing example

--- assignments/modularizing/parent.py
+++ assignments/modularizing/parent.py
@@ -7,7 +7,17 @@ class User:
     def say_hello(self):
         return 'Hello'
 
-print(square(5))
-user = User("Luigi")
-print(user.name)
-print(user.say_hello())
+# This conditional allows us to run certain blocks of code depending on which file they're in
+if __name__ == "__main__":
+    print('the file is being executed directly')
+else:
+    print('the file is being executed because it is imported by another file, this file is called', __name__)
+    pass
+
+
+
+# print(square(5))
+# user = User("Luigi")
+# print(user.name)
+# print(user.say_hello())
+# print(__name__)
